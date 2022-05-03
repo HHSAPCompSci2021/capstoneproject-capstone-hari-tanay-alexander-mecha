447b2a5d926af2580fb9e357d0b11d392fbbdf18
AwesomenessReborn <hareee234@gmail.com>
2022-05-03 10:02:15 -0700

    initial changes

---
+++ CapstoneProject/src/screens/SecondScreen.java
@@ -0,0 +1,45 @@
+package screens;
+
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import core.DrawingSurface;
+
+
+public class SecondScreen extends Screen {
+	
+	private DrawingSurface surface;
+	
+	private Rectangle screenRect;
+
+	
+	public SecondScreen(DrawingSurface surface) {
+		super(800,600);
+		
+	}
+
+
+	public void spawnNewMario() {
+		
+	}
+
+	// The statements in the setup() function 
+	// execute once when the program begins
+	public void setup() {
+		
+	}
+
+	// The statements in draw() are executed until the 
+	// program is stopped. Each statement is executed in 
+	// sequence and after the last line is read, the first 
+	// line is executed again.
+	public void draw() {
+		
+	}
+
+	
+}
