c1b5a2647df0642bdba1cb217e22c2db87084c05
Tanay Bartwal <tbartwal666@student.fuhsd.org>
2022-05-06 10:02:40 -0700

    Update Mech.java

--- CapstoneProject/src/Mechs/Mech.java
+++ CapstoneProject/src/Mechs/Mech.java
@@ -1,5 +1,41 @@
 package Mechs;
+import processing.core.PApplet;
 
 public class Mech {
-	//super
+	
+	private float xpos, ypos;
+	private int health, movementSpeed;
+	
+	public Mech(float x, float y, int h, int mS) {
+		xpos = x;
+		ypos = y;
+		health = h;
+		movementSpeed = mS;
+	}
+	
+	public float getX() {
+		return xpos;
+	}
+	
+	public float getY() {
+		return ypos;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public int getMovementSpeed() {
+		return movementSpeed;
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void changePos(int change) {
+		if(keyPressed) {
+			
+		}
+	}
 }
