fb07f8093bec8a3f3139118d11c969a64f30a4de
Alex Yue <ayue595@student.fuhsd.org>
2022-05-10 11:43:24 -0700

    Bullet

---
+++ CapstoneProject/src/utility/field/projectiles/Bullet.java
@@ -0,0 +1,12 @@
+package utility.field.projectiles;
+
+public class Bullet extends Projectile {
+	
+	private float dmg, range;
+
+	public Bullet(float x, float y, int damage, double v) {
+		super(x, y, damage, v);
+		
+	}
+
+}
