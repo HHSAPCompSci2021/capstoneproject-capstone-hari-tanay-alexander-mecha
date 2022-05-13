ba464a8a639ffbe8f297bf79aeb0b4d4e921e39f
Alex Yue <ayue595@student.fuhsd.org>
2022-05-12 21:56:04 -0700

    enemy class

---
+++ CapstoneProject/src/screens/ShopScreen.java
@@ -0,0 +1,21 @@
+package screens;
+
+import core.DrawingSurface;
+
+public class ShopScreen extends Screen {
+	
+	private DrawingSurface surface;
+
+	public ShopScreen(DrawingSurface surface) {
+		super(800, 600);
+		this.surface = surface;
+	}
+
+	public void draw() {
+		
+	}
+	
+	public void mousePressed() {
+		
+	}
+}
